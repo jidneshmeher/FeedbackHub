f3c6060f8451ac04aba4dff93fd50ce0eaf00134
Jidnesh Meher <meherjidnesh89@gmail.com>
2025-09-02 00:06:11 +0530

    fix: Add Connection String for Database

--- server/index.js
+++ server/index.js
@@ -8,11 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.connect()
